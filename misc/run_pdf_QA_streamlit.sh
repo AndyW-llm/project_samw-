f703df3000b5237bfed3eb5eddea626614e0e0ea
andywongpaii <andy.wong.paii@gmail.com>
2024-03-17 17:54:17 -0700

    backup

--- misc/run_pdf_QA_streamlit.sh
+++ misc/run_pdf_QA_streamlit.sh
@@ -1,6 +1,10 @@
+# ssh -L 8000:localhost:8000 e97
+# conda activate qa_demo
+
 # Changing the directory
 cd /Users/andywong/andyw/side_projects/project_samw-/streamlit
 
 # Running the codecracker command
 export STREAMLIT_SERVER_ENABLE_STATIC_SERVING=true
+export TOKENIZERS_PARALLELISM=false
 streamlit run ./pdf_QA_app.py
